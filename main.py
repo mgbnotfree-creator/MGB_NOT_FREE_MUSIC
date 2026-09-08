efbad9d6fa0234cbc59c5573d5eef26098456988
mgbnotfree-creator <mgbnotfree@gmail.com>
2026-09-09 00:31:34 +0530

    update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import asyncio
 import os
 from flask import Flask
 from threading import Thread
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrofork import Client, filters
+from pyrofork.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pytgcalls import PyTgCalls
 from pytgcalls.types.input_stream import AudioPiped
 import yt_dlp
