8d987faa86aa88a9bde45aa051ce9a14cdacc090
mgbnotfree-creator <mgbnotfree@gmail.com>
2026-09-11 18:34:05 +0530

    update web.py

--- web.py
+++ web.py
@@ -1,3 +1,18 @@
+# ==========================================================
+# Copyright (c) 2026 MGB Not Free Creator 
+# All Rights Reserved.
+#
+# Project      : MGB Not Free Music Telegram Bot
+# Powered By   : MGB Not Free Creator
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @MUSIC1_NOT_FREE_BOT
+# Channel      : https://t.me/MUSIC_SUPPORT_69
+# GitHub       : https://github.com/mgbnotfree-creator/MGB_NOT_FREE_MUSIC
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import threading
@@ -30,3 +45,4 @@ def run():
 
 if __name__ == "__main__":
     run()
+    
