23c9f659994d592c4203fa5e2792f08ba6124d22
mgbnotfree-creator <mgbnotfree@gmail.com>
2026-09-11 17:59:47 +0530

    create dir.py

---
+++ Elevenyts/core/Elevenyts/core/Elevenyts/core/dir.py
@@ -0,0 +1,17 @@
+# ==========================================================
+# Copyright (c) 2026 VelocityBots 
+# All Rights Reserved.
+#
+# Project      : VelocityBots API Telegram Music Bot
+# Powered By   : VelocityBots 
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @MUSIC1_NOT_FREE_BOT
+# Channel      : https://t.me/MUSIC_SUPPORT_69
+# GitHub       : https://mgbnotfree-creator/MGB_NOT_FREE_MUSIC
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
+import base64
+exec(base64.b64decode("IyA9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09CiMgQ29weXJpZ2h0IChjKSAyMDI2IFZlbG9jaXR5Qm90cwojIEFsbCBSaWdodHMgUmVzZXJ2ZWQuCiMKIyBQcm9qZWN0ICAgICAgOiBWZWxvY2l0eUJvdHMg6q2ZIE11c2ljIFRlbGVncmFtIEJvdAojIFBvd2VyZWQgQnkgICA6IEFydGlzdAojIFR5cGUgICAgICAgICA6IEFQSSBCYXNlZCBUZWxlZ3JhbSBNdXNpYyBCb3QKIwojIEJvdCAgICAgICAgICA6IEBBcnRpc3RBcGlib3QKIyBDaGFubmVsICAgICAgOiBodHRwczovL3QubWUvYXJ0aXN0Ym90cwojIEdpdEh1YiAgICAgICA6IGh0dHBzOi8vZ2l0aHViLmNvbS9lbGV2ZW55dHMKIwojIFVuYXV0aG9yaXplZCBjb3B5aW5nLCBtb2RpZmljYXRpb24sIG9yIHJlZGlzdHJpYnV0aW9uCiMgb2YgdGhpcyBzb3VyY2UgY29kZSB3aXRob3V0IHBlcm1pc3Npb24gaXMgcHJvaGliaXRlZC4KIyA9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09CmZyb20gcGF0aGxpYiBpbXBvcnQgUGF0aAoKZnJvbSBFbGV2ZW55dHMgaW1wb3J0IGxvZ2dlcgoKCmRlZiBlbnN1cmVfZGlycygpOgogICAgIiIiCiAgICBDcmVhdGUgbmVjZXNzYXJ5IGRpcmVjdG9yaWVzIGlmIHRoZXkgZG9uJ3QgZXhpc3QuCgogICAgQ3JlYXRlczoKICAgIC0gY2FjaGUvOiBGb3IgdGVtcG9yYXJ5IGNhY2hlIGZpbGVzCiAgICAtIGRvd25sb2Fkcy86IEZvciBkb3dubG9hZGVkIG1lZGlhIGZpbGVzCiAgICAiIiIKICAgICMgTGlzdCBvZiByZXF1aXJlZCBkaXJlY3RvcmllcwogICAgZm9yIGRpciBpbiBbImNhY2hlIiwgImRvd25sb2FkcyJdOgogICAgICAgICMgQ3JlYXRlIGRpcmVjdG9yeSAoYW5kIHBhcmVudHMgaWYgbmVlZGVkKQogICAgICAgIFBhdGgoZGlyKS5ta2RpcihwYXJlbnRzPVRydWUsIGV4aXN0X29rPVRydWUpCiAgICBsb2dnZXIuaW5mbygi8J+TgSBDYWNoZSBkaXJlY3RvcmllcyB1cGRhdGVkLiIpCg==").decode("utf-8"))
