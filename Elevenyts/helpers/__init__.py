61298dfa961b0f07a8a31feee63e508aaa847787
mgbnotfree-creator <mgbnotfree@gmail.com>
2026-09-14 15:55:54 +0530

    create __init__.py

---
+++ Elevenyts/helpers/__init__.py
@@ -0,0 +1,27 @@
+# ==========================================================
+# Copyright (c) 2026 MGB NOT FREE 
+# All Rights Reserved.
+#
+# Project      : mgb not free
+# Powered By   : MGB NOT FREE 
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @RIYA_MUSIC_X_BOT
+# Channel      : https://t.me/RIYA_MUSIC_BOT_786
+# GitHub       : https://github.com/mgbnotfree-creator/MGB_NOT_FREE_MUSIC
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
+from ._admins import admin_check, can_manage_vc, is_admin, reload_admins, can_manage_vc_channel
+from ._dataclass import Media, Track
+from ._emoji import next_play_emoji
+from ._exec import format_exception, meval
+from ._inline import Inline
+from ._queue import Queue
+from ._thumbnails import Thumbnail
+from ._utilities import Utilities
+
+buttons = Inline()
+thumb = Thumbnail()
+utils = Utilities()
