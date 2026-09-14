afa419474850f398fc0610f912430c85f13a0f47
mgbnotfree-creator <mgbnotfree@gmail.com>
2026-09-14 16:16:06 +0530

    create _dataclass.py

---
+++ Elevenyts/helpers/_dataclass.py
@@ -0,0 +1,51 @@
+# ==========================================================
+# Copyright (c) 2026 MGB NOT FREE 
+# All Rights Reserved.
+#
+# Project      : mgb not free
+# Powered By   : MGB NOT FREE 
+# Type         : API Based Telegram Music Bot
+#
+# Bot          : @RIYA_MUSIC_X_BOT
+# Channel      : https://t.me/RIYA_MUSIC_BOT_786
+# GitHub       : https://github.com/mgbnotfree-creator/MGB_NOT_FREE_MUSIC
+#
+# Unauthorized copying, modification, or redistribution
+# of this source code without permission is prohibited.
+# ==========================================================
+from dataclasses import dataclass
+
+
+@dataclass
+class Media:
+    id: str
+    duration: str
+    duration_sec: int
+    file_path: str
+    message_id: int
+    title: str
+    url: str
+    time: int = 0
+    user: str = None
+    is_live: bool = False
+    ytitle: str = None
+    video: bool = False  # 🔙 ADDED
+
+
+@dataclass
+class Track:
+    id: str
+    channel_name: str
+    duration: str
+    duration_sec: int
+    title: str
+    url: str
+    file_path: str = None
+    message_id: int = 0
+    time: int = 0
+    thumbnail: str = None
+    user: str = None
+    view_count: str = None
+    is_live: bool = False
+    ytitle: str = None
+    video: bool = False  # 🔙 ADDED
